e1688ae0be59d4b09d5270e59cdd88cb0529931d
BCA-krishna <krishnapatel1284@gmail.com>
2025-09-18 15:15:02 +0530

    Removed multiple Python files from repository

--- reversedArray.py
+++
@@ -1,5 +0,0 @@
-A=list(map(int,input().split()))
-n=len(A)
-for i in range(0,n//2):
-    A[i],A[n-i-1]=A[n-i-1],A[i]
-print(A)
--- searchElement.py
+++
@@ -1,6 +0,0 @@
-A=list(map(int,input().split()))
-B=int(input("enter search"))
-for i in range(len(A)):
-    if(A[i]==B):
-        print(i)
-        break
